a35dc74d6d5de89c769e324f85688977a15fb43f
seungheonL <44883967+seungheonL@users.noreply.github.com>
2021-09-26 00:00:47 +0900

    Create 15650_N과_M_(2).cpp

---
+++ backtracking/15650_N과_M_(2).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> v;
+
+int N, M;
+
+void print(int index, int size)
+{
+    if(size == M)
+    {
+        for(int i=0; i<M; i++)
+        {
+            cout << v[i] << " ";
+        }
+        cout << endl;
+        
+        return;
+    }
+    
+    for(int i=index; i<=N; i++)
+    {
+        v.push_back(i);
+        print(i + 1, size + 1);
+        v.pop_back();
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+    
+    print(1, 0);
+    
+    return 0;
+}
