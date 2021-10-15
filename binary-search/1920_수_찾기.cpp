11bc0eee775022db3cf1295543cc3860a8456009
seungheonL <44883967+seungheonL@users.noreply.github.com>
2021-10-15 18:47:37 +0900

    Create 1920_수_찾기.cpp

---
+++ binary-search/1920_수_찾기.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector<int> list;
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		scanf("%d", &num);
+		list.push_back(num);
+	}
+
+	sort(list.begin(), list.end());
+
+	int M;
+	cin >> M;
+
+	for (int i = 0; i < M; i++)
+	{
+		int num;
+		scanf("%d", &num);
+
+		printf("%d\n",binary_search(list.begin(), list.end(), num));
+	}
+
+	return 0;
+}
